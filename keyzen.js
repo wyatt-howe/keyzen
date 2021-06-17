ad5fc1dbd72fb4c3925038227cf71101dc38ad39
Wyatt Howe <43802211+wyatt-howe@users.noreply.github.com>
2021-06-17 14:57:26 -0400

    Change the default layout to QGMLWY

--- keyzen.js
+++ keyzen.js
@@ -5,7 +5,7 @@ var start_time = 0;
 var hpm = 0;
 var ratio = 0;
 
-data.chars = " jfkdlsahgyturieowpqbnvmcxz6758493021`-=[]\\;',./ABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+{}|:\"<>?";
+data.chars = " anetospqirywflumbzhdjkvcxg6758493021'\",.!?:;/@$%&#*()_ABCDEFGHIJKLMNOPQRSTUVWXYZ~+-={}|^<>`[]\\";
 data.consecutive = 5;
 data.word_length = 7;
 
